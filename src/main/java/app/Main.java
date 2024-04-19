e65ce58c8ecc00d6c8b44c7541d91f54839d6c2d
novikov7ua <novikov7ua@gmail.com>
2024-04-19 19:16:22 +0300

    Lesson 3 - Part 2

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,7 +2,7 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        String string = "This is shop-online app";
+        String string = "Shop-online for customers";
         System.out.println(string);
     }
 }
