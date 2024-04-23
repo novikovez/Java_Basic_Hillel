cffb165e32ae8cb1e89866cfcc1113dc96cf457b
novikov7ua <novikov7ua@gmail.com>
2024-04-23 22:33:59 +0300

    Lesson 4- UPD

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -9,17 +9,25 @@ public class Main {
     private static final String salesDay = "Sales by day is EUR %.2f%n";
     private static final String salesMonth = "Sales by month is EUR %.2f%n"; // від себе
 
+
     public static void main(String[] args) {
-        showConsoleData(1, 5, "smartphone", 12153.41F);
-        showConsoleData(2, 7, "laptop", 10486.85F);
+        showConsoleData(1, 5, "smartphone", 121.53F, 74);
+        showConsoleData(2, 7, "laptop", 104.86F, 123);
     }
 
-    private static void showConsoleData(int id, int days, String product, float total) {
+    private static void showConsoleData(
+            int id,
+            int days,
+            String product,
+            float price,
+            int pcs
+    ) {
         LocalDate currentDate = LocalDate.now();
         int daysInMonth = currentDate.lengthOfMonth();
 
-        float avgDay = total / days;
-        float avgMonth = avgDay * daysInMonth;
+        float total = price * pcs;
+        float totalDays = total / days;
+        float avgMonth = totalDays * daysInMonth;
 
         System.out.printf(
                 productTitle
@@ -27,7 +35,7 @@ public class Main {
                         + daysEur
                         + salesDay
                         + salesMonth,
-                id, product, days, total, avgDay, avgMonth
+                id, product, days, total, totalDays, avgMonth
         );
     }
 }
