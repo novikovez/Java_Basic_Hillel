1f2bb9542fca092f9874c3da80558a48f523aa11
novikov7ua <novikov7ua@gmail.com>
2024-04-23 21:24:02 +0300

    Lesson 4

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,33 @@
 package app;
 
+import java.time.LocalDate;
+
 public class Main {
+    private static final String productTitle = "Product No %d: %s%n";
+    private static final String totalSaleTitle = "Total sales for %d";
+    private static final String daysEur = " days is EUR %.2f%n";
+    private static final String salesDay = "Sales by day is EUR %.2f%n";
+    private static final String salesMonth = "Sales by month is EUR %.2f%n"; // від себе
+
     public static void main(String[] args) {
-        String string = "Shop-online for customers";
-        System.out.println(string);
+        showConsoleData(1, 5, "smartphone", 12153.41F);
+        showConsoleData(2, 7, "laptop", 10486.85F);
+    }
+
+    private static void showConsoleData(int id, int days, String product, float total) {
+        LocalDate currentDate = LocalDate.now();
+        int daysInMonth = currentDate.lengthOfMonth();
+
+        float avgDay = total / days;
+        float avgMonth = avgDay * daysInMonth;
+
+        System.out.printf(
+                productTitle
+                        + totalSaleTitle
+                        + daysEur
+                        + salesDay
+                        + salesMonth,
+                id, product, days, total, avgDay, avgMonth
+        );
     }
 }
