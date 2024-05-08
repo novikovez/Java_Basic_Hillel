05b82f878566ccc59eea74a0fb3e1c53c8a216db
novikov7ua <novikov7ua@gmail.com>
2024-05-08 19:45:46 +0300

    Binary Search & Insertion Sort

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,46 +1,35 @@
 package app;
 
 import java.util.Arrays;
-import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
-    private static final Locale LOCALE = Locale.US;
-    private static final int[] array = new int[20];
-    private static final int min = -100;
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final int[] array = new int[15];
+    private static final int min = 1;
     private static final int max = 100;
-    private static int negativeTotal;
-    private static int pairedCount;
-    private static int unpairedCount;
-    private static int maxValue = 0;
-    private static int maxValueKey;
-    private static int minValue = 0;
-    private static int minValueKey;
-    private static int point6Index = -1;
-    private static final String string1 = "Найбільший елемент: %d (з індексом %d)%n";
-    private static final String string2 = "Найменший елемент: %d (з індексом %d)%n";
-    private static final String string3 = "Кількість непарних чисел: %d%n";
-    private static final String string4 = "Кількість парних чисел: %d%n";
-    private static final String string5 = "Сума від'ємних чисел: %d%n";
-    private static final String string6 = "Елементи масиву: ";
-    private static final String string7 = "Середнє арифметичне чисел після першого від'ємного числа: %.2f%n";
+    private static int input;
+
+    private static final String string1 = "Початковий вигляд масиву: ";
+    private static final String string2 = "Відсортований масив:";
+    private static final String string3 = "Введіть число для пошуку: ";
+    private static final String string4 = "Індекс числа: %d у відсортованому масиві: %d";
+    private static final String string5 = "Число %d не знайдено у відсортованому масиві";
+
 
     public static void main(String[] args) {
         setDataArray();
-        System.out.println(string6 + Arrays.toString(array));
-
-        for(int i = 0; i < array.length; i++) {
-            negativeTotal(array[i]);
-            pairedAndUnpaired(array[i]);
-            maxAndMinValue(array[i], i);
-            point6SetIndex(array[i], i);
+        System.out.println(string1 + Arrays.toString(array));
+        insertionSort();
+        System.out.println(string2 + Arrays.toString(array));
+        System.out.println(string3);
+        input = scanner.nextInt();
+        int binarySearchResult = binarySearch();
+        if(binarySearchResult != -1) {
+            System.out.printf(string4, input, binarySearchResult);
+        } else {
+            System.out.printf(string5, input);
         }
-
-        System.out.printf(string5, negativeTotal);
-        System.out.printf(string4, pairedCount);
-        System.out.printf(string3, unpairedCount);
-        System.out.printf(string2, minValue, minValueKey);
-        System.out.printf(string1, maxValue, maxValueKey);
-        System.out.println(point6());
     }
 
     private static void setDataArray() {
@@ -49,56 +38,34 @@ public class Main {
         }
     }
 
-    private static void negativeTotal(int value) {
-        if(value < 0) {
-            negativeTotal += value;
+    private static void insertionSort() {
+        int n = array.length;                       // розмір масиву
+        for (int i = 1; i < n; ++i) {               // порівнюємо з минулим, тому починемо з індексу 1
+            int key = array[i];                     // беремо елемент із масиву
+            int j = i - 1;                          // індекс минулого значення
+
+            while (j >= 0 && array[j] > key) {      // входимо якщо минуле значення більше поточного
+                array[j + 1] = array[j];            // перемістити минуле значення вперед
+                j = j - 1;                          // беремо значення зліва і перевіряємо у while
+            }
+            array[j + 1] = key;                     // елементів для перемішування немає, поточний елемент, у правильне місце
         }
     }
 
-    private static void pairedAndUnpaired(int value) {
-        if(value % 2 == 0) {
-            pairedCount++;
-        } else {
-            unpairedCount++;
-        }
-    }
-
-    private static void maxAndMinValue(int value, int key) {
-        if(maxValue == 0 || maxValue < value) {
-            maxValue = value;
-            maxValueKey = key;
-        }
-        if(minValue == 0 || minValue > value) {
-            minValue = value;
-            minValueKey = key;
-        }
-    }
-
-    private static void point6SetIndex(int value, int key) {
-        if(point6Index == -1) {
-            if(value < 0) {
-                point6Index = key + 1;
+    private static int binarySearch() {
+        int left = 0;                               // початковий індекс
+        int right = array.length - 1;               // останній індекс
+        while (left <= right) {                     // поки left не стане більше
+            int mid = left + (right - left) / 2;    // індекс середнього елемента
+            if (array[mid] == input) {              // перевірка середнього елемента з пошуком
+                return mid;                         // якщо знайшли, повернути
+            } else if (array[mid] < input) {        // перевірити чи більше пошук середнього значення, якщо так, пошук в right
+                left = mid + 1;                     // якщо більше, значення в right, новий початковий кордон
+            } else {
+                right = mid - 1;                    // новий кінцевий кордон
             }
         }
+        return -1;                                  // не знайшли число
     }
-
-    private static String point6() {
-        if(point6Index == -1) {
-            return "Від'ємних чисел немає";
-        }
-
-        int count = 0;
-        int total = 0;
-
-        for(int i = point6Index; i < array.length; i++) {
-            count++;
-            total += array[i];
-        }
-
-        float result = (float) total / count;
-        return String.format(LOCALE, string7, result);
-    }
-
-
 }
 
