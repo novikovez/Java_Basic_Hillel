31a40a38e1d0d800a5087eb6e7c7e3517bf7d752
novikov7ua <novikov7ua@gmail.com>
2024-05-10 23:18:46 +0300

    ДЗ 9.1. Робота з багатовимірними масивами

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,71 +1,128 @@
 package app;
 
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
-    private static final Scanner scanner = new Scanner(System.in);
-    private static final int[] array = new int[15];
-    private static final int min = 1;
-    private static final int max = 100;
-    private static int input;
+    private static final int[][] array = new int[4][4];
+    private static final Random random = new Random();
 
-    private static final String string1 = "Початковий вигляд масиву: ";
-    private static final String string2 = "Відсортований масив:";
-    private static final String string3 = "Введіть число для пошуку: ";
-    private static final String string4 = "Індекс числа: %d у відсортованому масиві: %d";
-    private static final String string5 = "Число %d не знайдено у відсортованому масиві";
-
+    private static final String string1 = "Матриця 4x4:";
+    private static final String string2 = "Сума елементів у парних рядках (рядок 0, 2): %d%n";
+    private static final String string3 = "Сума елементів у непарних рядках (рядок 1, 3): %d%n";
+    private static final String string4 = "Добуток елементів у парних стовпцях (стовпець 0, 2): %.0f%n";
+    private static final String string5 = "Добуток елементів у непарних стовпцях (стовпець 1, 3): %.0f%n";
+    private static final String string6 = "Матриця не є магічним квадратом";
+    private static final String string7 = "Матриця є магічним квадратом";
 
     public static void main(String[] args) {
         setDataArray();
-        System.out.println(string1 + Arrays.toString(array));
-        insertionSort();
-        System.out.println(string2 + Arrays.toString(array));
-        System.out.println(string3);
-        input = scanner.nextInt();
-        int binarySearchResult = binarySearch();
-        if(binarySearchResult != -1) {
-            System.out.printf(string4, input, binarySearchResult);
-        } else {
-            System.out.printf(string5, input);
-        }
+        System.out.println(string1);
+        showArray();
+
+        int[] sumPairedUnPaired = sumPairedAndUnPaired();
+        System.out.printf(string2, sumPairedUnPaired[0]);
+        System.out.printf(string3, sumPairedUnPaired[1]);
+
+        float[] multiplicationArray = multiplication();
+        System.out.printf(string4, multiplicationArray[0]);
+        System.out.printf(string5, multiplicationArray[1]);
+
+        System.out.print(isMagicSquare());
+
     }
 
     private static void setDataArray() {
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * (max - min + 1) + min);
+            for (int i2 = 0; i2 < array[i].length; i2++) {
+                array[i][i2] = random.nextInt(1, 50);
+            }
         }
     }
 
-    private static void insertionSort() {
-        int n = array.length;                       // розмір масиву
-        for (int i = 1; i < n; ++i) {               // порівнюємо з минулим, тому починемо з індексу 1
-            int key = array[i];                     // беремо елемент із масиву
-            int j = i - 1;                          // індекс минулого значення
-
-            while (j >= 0 && array[j] > key) {      // входимо якщо минуле значення більше поточного
-                array[j + 1] = array[j];            // перемістити минуле значення вперед
-                j = j - 1;                          // беремо значення зліва і перевіряємо у while
-            }
-            array[j + 1] = key;                     // елементів для перемішування немає, поточний елемент, у правильне місце
+    private static void showArray() {
+        for (int[] arr : array) {
+            System.out.println(Arrays.toString(arr));
         }
     }
 
-    private static int binarySearch() {
-        int left = 0;                               // початковий індекс
-        int right = array.length - 1;               // останній індекс
-        while (left <= right) {                     // поки left не стане більше
-            int mid = left + (right - left) / 2;    // індекс середнього елемента
-            if (array[mid] == input) {              // перевірка середнього елемента з пошуком
-                return mid;                         // якщо знайшли, повернути
-            } else if (array[mid] < input) {        // перевірити чи більше пошук середнього значення, якщо так, пошук в right
-                left = mid + 1;                     // якщо більше, значення в right, новий початковий кордон
-            } else {
-                right = mid - 1;                    // новий кінцевий кордон
+    private static int[] sumPairedAndUnPaired() {
+        int[] result = new int[2];
+        for (int[] arr : array) {
+            for (int i = 0; i < arr.length; i++) {
+                if (i % 2 == 0) {
+                    result[0] += arr[i];
+                } else {
+                    result[1] += arr[i];
+                }
             }
         }
-        return -1;                                  // не знайшли число
+        return result;
+    }
+
+    private static float[] multiplication() {
+        float[] result = {1, 1};
+        for (int i = 0; i < array.length; i++) {
+            for (int i2 = 0; i2 < array[i].length; i2++) {
+                if (i2 % 2 == 0) {
+                    result[0] *= array[i2][i];
+                } else {
+                    result[1] *= array[i2][i];
+                }
+            }
+        }
+        return result;
+    }
+
+    public static String isMagicSquare() {
+        int n = array.length;                           // длина массива
+        for (int[] ints : array) {                      // проверка на квадрат
+            if (ints.length != n) {                     // если длина подмассива не равна длине массива - false
+                return string6;
+            }
+        }
+
+        int sum = 0;                                    // сумма первой строки для следующих проверок
+        for (int j = 0; j < n; j++) {
+            sum += array[0][j];
+        }
+
+        for (int i = 1; i < n; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < n; j++) {
+                rowSum += array[i][j];
+            }
+            if (rowSum != sum) {                        // сравнение остальных строк с первой
+                return string6;
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            int colSum = 0;
+            for (int i = 0; i < n; i++) {
+                colSum += array[i][j];
+            }
+            if (colSum != sum) {                        // Проверяем суммы столбцов с суммой первой строки
+                return string6;
+            }
+        }
+
+        int diagSum = 0;
+        for (int i = 0; i < n; i++) {
+            diagSum += array[i][i];
+        }
+        if (diagSum != sum) {                           // Проверяем сумму главной диагонали
+            return string6;
+        }
+
+        int antiDiagSum = 0;
+        for (int i = 0; i < n; i++) {
+            antiDiagSum += array[i][n - i - 1];         // Проверяем сумму побочной диагонали
+        }
+        if (antiDiagSum != sum) {
+            return string6;
+        }
+        return string7;                                 // Если все суммы равны, то это магический квадрат
     }
 }
 
