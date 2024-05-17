2982e6dafc7fc76d4de4c9b9bab8e35cb7e86393
Igor Novikov <novikov7ua@gmail.com>
2024-05-17 20:47:04 +0300

    OOP

--- src/main/java/app/Person.java
+++ src/main/java/app/Person.java
@@ -5,6 +5,12 @@ public class Person {
     private int age;
     private String work;
 
+    public Person(String name, int age, String work) {
+        this.name = name;
+        this.age = age;
+        this.work = work;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -14,12 +20,6 @@ public class Person {
     }
 
     public void setWork(String work) {
-        this.work = work;
-    }
-
-    public Person(String name, int age, String work) {
-        this.name = name;
-        this.age = age;
         this.work = work;
     }
 
