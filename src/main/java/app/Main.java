4666bb4295a827c92446326fbcbceccff3a206b7
Igor Novikov <novikov7ua@gmail.com>
2024-05-17 19:48:03 +0300

    OOP

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,84 +1,23 @@
 package app;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
-
 public class Main {
-    private static final Scanner scanner = new Scanner(System.in);
-    private static final String string1 = "Введіть ціле число: ";
-    private static final String string2 = "Квадрат числа %d дорівнює %d%n";
-    private static final String string3 = "Об'єм циліндра з радіусом %.2f і висотою %.2f дорівнює %f%n";
-    private static final String string4 = "Масив чисел: %s%n";
-    private static final String string5 = "Сума всіх елементів масиву дорівнює: %d%n";
-    private static final String string6 = "Введіть рядок:";
-    private static final String string7 = "Рядок в зворотньому порядку: %s%n";
-    private static final String string8 = "Введіть a:";
-    private static final String string9 = "Введіть b:";
-    private static final String string10 = "Результат %d^%d дорівнює %d%n";
-
+    private static final String string1 = "Ім'я: %s, Вік: %d, Професія: %s%n";
+    private static final String string2 = "(Після оновлення професії)";
 
     public static void main(String[] args) {
-        System.out.print(string1);
-        int input = scanner.nextInt();
-        System.out.printf(string2, input, square(input));
+        Person person1 = new Person("John", 30, "Інженер");
+        Person person2 = new Person("Mary", 25, "Вчитель");
+        Person person3 = new Person("Bob", 35, "Лікар");
 
-        double cylinderRadius = 3.5;
-        double cylinderHeight = 10.0;
-        System.out.printf(string3, cylinderRadius, cylinderHeight, cylinderVolume(cylinderRadius, cylinderHeight));
+        System.out.printf(string1, person1.getName(), person1.getAge(), person1.getWork());
+        System.out.printf(string1, person2.getName(), person2.getAge(), person2.getWork());
+        System.out.printf(string1, person3.getName(), person3.getAge(), person3.getWork());
 
-        int[] arrayInt = {10, 20, 30, 40, 50};
-        System.out.printf(string4, Arrays.toString(arrayInt));
-        System.out.printf(string5, arrayOfIntegers(arrayInt));
+        person3.setWork("Президент");
 
-        scanner.nextLine();
-        System.out.print(string6);
-        String inputString = scanner.nextLine();
-        System.out.printf(string7, string(inputString));
-
-        System.out.print(string8);
-        int a = scanner.nextInt();
-        System.out.print(string9);
-        int b = scanner.nextInt();
-        System.out.printf(string10, a, b, degree(a, b));
-
-        System.out.print(string1);
-        int value = scanner.nextInt();
-        System.out.print(string6);
-        scanner.nextLine();
-        String string = scanner.nextLine();
-        stringAndInt(value, string);
-
+        System.out.println(string2);
+        System.out.printf(string1, person3.getName(), person3.getAge(), person3.getWork());
     }
 
-    private static int square(int value) {
-        return value * value;
-    }
-
-    private static double cylinderVolume(double cylinderRadius, double cylinderHeight) {
-        return Math.PI * cylinderRadius * cylinderRadius * cylinderHeight;
-    }
-
-    private static int arrayOfIntegers(int[] array) {
-        int sum = 0;
-        for (int j : array) {
-            sum = sum + j;
-        }
-        return sum;
-    }
-
-    private static String string(String string) {
-        return new StringBuilder(string).reverse().toString();
-    }
-
-    private static int degree(int a, int b) {
-        return (int) Math.pow(a, b);
-    }
-
-    private static void stringAndInt(int value, String string) {
-        for (int i = 0; i < value; i++) {
-            System.out.println(string);
-        }
-    }
 }
 
---
+++ src/main/java/app/Person.java
@@ -0,0 +1,38 @@
+package app;
+
+public class Person {
+    private String name;
+    private int age;
+    private String work;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public Person(String name, int age, String work) {
+        this.name = name;
+        this.age = age;
+        this.work = work;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+}
